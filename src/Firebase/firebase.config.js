45e8bd8acc4136294e83d7a59c612ee2a61de6a7
kanak56 <ckanak40@gmail.com>
2021-10-29 21:02:39 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVjdj9eTSlpOhBL7kmIIgFJhq0quOYyUI",
+    authDomain: "yourtravelagency-6115b.firebaseapp.com",
+    projectId: "yourtravelagency-6115b",
+    storageBucket: "yourtravelagency-6115b.appspot.com",
+    messagingSenderId: "124979861300",
+    appId: "1:124979861300:web:13bd79dff074dbd67a7761"
+};
+
+export default firebaseConfig;
